8a04e445ef1453a61113a9a5b4535cfee7bc78f5
Luiz Davila <luizdavilaf@gmail.com>
2022-11-08 15:33:04 -0300

    vai

--- src/Cliente.java
+++ src/Cliente.java
@@ -16,16 +16,12 @@ public class Cliente {
 			String nome=null;
 			byte [] line = new byte[100];
 			do{
-				
-				System.out.println("Digite seu nome");
 				System.out.println("nome"+nome);
+				System.out.println("Digite seu nome");				
 				System.in.read(line);
 				o.write(line);
 				i.read(line);
 				nome = new String(nome);
-
-				
-									
 			}while(nome==null);
 		}
 		catch (Exception err){
@@ -46,34 +42,7 @@ public class Cliente {
 
 
 	public static void main(String[] args) {
-		regScan = new Scanner(System.in);
-		System.out.println("Digite seu nome");
-		name = regScan.nextLine();
-
-
-		
-
-
-
-
-		try{
-			Socket c = new Socket("192.168.0.53", 50001);
-			InputStream i = c.getInputStream();
-			OutputStream o = c.getOutputStream();
-			String str;
-			while(c.isConnected()){
-				byte [] line = new byte[1000];				
-				i.read(line);
-				str = new String(line);
-				System.out.println(str);
-				
-				
-			}
-			
-		
-		}
-		catch (Exception err){
-			System.err.println(err);
-		}
+		Cliente cliente = new Cliente();
+		cliente.connect();
 	}
 }
